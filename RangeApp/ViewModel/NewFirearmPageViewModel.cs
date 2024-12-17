426851c3c13d478dbf529978f9028a9f9bf922a8
Travis Clark <tlclark997@gmail.com>
2024-12-17 16:10:55 -0600

    Refactored NewFirearmPage

--- RangeApp/ViewModel/NewFirearmPageViewModel.cs
+++ RangeApp/ViewModel/NewFirearmPageViewModel.cs
@@ -4,38 +4,164 @@ using CommunityToolkit.Mvvm.Input;
 
 namespace RangeApp.ViewModel;
 
-public partial class NewFirearmPageViewModel : ObservableObject 
+public partial class NewFirearmPageViewModel : ObservableObject, IQueryAttributable
 {
     public NewFirearmPageViewModel()
     {
         AvailableFirearms = new ObservableCollection<string>(App.FirearmRepo.GetAllFirearmNames());
-
         StatusMessage = App.FirearmRepo.StatusMessage;
     }
 
-    [ObservableProperty]
-    public string caliber = string.Empty;
+    public void ApplyQueryAttributes(IDictionary<string, object> attributes)
+    {
+        if (attributes == null)
+            return;
+
+        // catches return from adding/updating a firearm
+        // sets the selected firearm to the one that was added/updated
+        if (attributes.ContainsKey("Firearm"))
+        {
+            var result = attributes["Firearm"] as ViewModel.FirearmData;
+            if (result != null)
+            {
+                firearm = result;
+                setData();
+            }
+
+        }
+    }
+
     [ObservableProperty]
     ObservableCollection<Models.Firearm>? allFirearms;
     [ObservableProperty]
     ObservableCollection<string>? availableFirearms;
     [ObservableProperty]
     string statusMessage = string.Empty;
+    [ObservableProperty]
+    string? name = string.Empty;
+    [ObservableProperty]
+    string? barrelLength = string.Empty;
+    [ObservableProperty]
+    string? manufacture = string.Empty;
+    [ObservableProperty]
+    string? caliber = string.Empty;
+    [ObservableProperty]
+    string? scopeId = string.Empty;
+    ViewModel.FirearmData? firearm;
 
+    /// <summary>
+    /// Sets the data fields based on the firearm data
+    /// </summary>
+    private void setData()
+    {
+        if (firearm == null)
+            return;
+        Name = firearm.Name;
+        BarrelLength = firearm.BarrelLength.ToString();
+        Manufacture = firearm.Manufacturer;
+        Caliber = firearm.Caliber;
+        ScopeId = firearm.ScopeID;
+    }
+    /// <summary>
+    /// Sets the status message for errors
+    /// </summary>
     public void SetStatusMessage(string message)
     {
         StatusMessage = message;
     }
-    public bool CheckNameIsDuplicate(string name)
+
+    /// <summary>
+    /// Checks that the name field is not the same name as a firearm already in
+    /// the database
+    /// </summary>
+    public bool CheckNameIsDuplicate()
     {
         if (AvailableFirearms != null)
         {
             for (int i = 0; i < AvailableFirearms.Count; i++)
             {
-                if (name == AvailableFirearms[i])
+                if (Name == AvailableFirearms[i])
                     return true;
             }
         }
         return false;
+    }
+
+    /// <summary>
+    /// Returns the firearm name that is being edited. If not editing a firearm
+    /// then null string is returned.
+    /// </summary>
+    public string? getFirearmName()
+    {
+        if (firearm != null)
+        {
+            // if the name is null, return an empty string
+            if (firearm.Name == null)
+                return string.Empty;
+            else
+                return firearm.Name;
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+    /// <summary>
+    /// Adds the firearm to the database and returns to the calling page. 
+    /// Passes the new Firearm to the calling page
+    /// </summary>
+    [RelayCommand]
+    void SaveFirearm()
+    {
+        // creates the Firearm object to save to the database
+        int barrelLength;
+        if (BarrelLength != null)
+            barrelLength = int.Parse(BarrelLength);
+        else
+            barrelLength = 0;
+        var new_firearm = new Models.Firearm();
+        new_firearm.Name = Name;
+        new_firearm.BarrelLength = barrelLength;
+        new_firearm.Manufacturer = Manufacture;
+        new_firearm.Caliber = Caliber;
+        new_firearm.ScopeID = ScopeId;
+        if (firearm != null)
+        {
+            new_firearm.Id = firearm.Id;
+        }
+
+        App.FirearmRepo.AddNewFirearm(new_firearm);
+
+        // Returns to the calling page
+        // if the page is editing a firearm
+        var return_data = ViewModel.FirearmData.GetData(new_firearm);
+        if (firearm != null)
+        {
+            var navigationParamenter = new Dictionary<string, object>
+            {
+                {"Firearm", return_data }
+            };
+            Shell.Current.GoToAsync("..", navigationParamenter);
+        }
+        // if the page is creating a firearm
+        else
+        {
+            var navigationParamenter = new Dictionary<string, object>
+            {
+                {"Firearm", return_data }
+            };
+            Shell.Current.GoToAsync("..", navigationParamenter);
+        }
+
+    }
+
+    /// <summary>
+    /// Cancels the creation of a new Firearm 
+    /// </summary>
+    [RelayCommand]
+    void Cancel()
+    {
+        Shell.Current.GoToAsync("..");
     }
 }
--- RangeApp/Views/NewFirearmPage.xaml.cs
+++ RangeApp/Views/NewFirearmPage.xaml.cs
@@ -4,270 +4,236 @@ namespace RangeApp.Views;
 
 public partial class NewFirearmPage : ContentPage
 {
-	public NewFirearmPage(ViewModel.NewFirearmPageViewModel vm)
-	{
-		InitializeComponent();
-		VM = vm;
-		BindingContext = VM;
-		textColor = NewFirearmName.TextColor;	
-		WeakReferenceMessenger.Default.Register<Models.SendItemMessage>(this, (r, m) =>
-            {
-				PopulateWithFirearm(m.Value);	
+    public NewFirearmPage(ViewModel.NewFirearmPageViewModel vm)
+    {
+        InitializeComponent();
+        VM = vm;
+        BindingContext = VM;
+        textColor = NewFirearmName.TextColor;
+    }
+    public NewFirearmPage()
+    {
+        InitializeComponent();
+        VM = new ViewModel.NewFirearmPageViewModel();
+        BindingContext = VM;
+        textColor = NewFirearmName.TextColor;
+        NewFirearmCaliber.Text = VM.Caliber;
+    }
+    private ViewModel.NewFirearmPageViewModel VM;
+    private Color? textColor = null;
+    private bool AddFirearmParamtersPass = false;
 
-            });
-	}
-	public NewFirearmPage()
-	{
-		InitializeComponent();
+    private bool NewFirearmNamePass = false;
 
-		VM = new ViewModel.NewFirearmPageViewModel();
-		BindingContext = VM;
-		textColor = NewFirearmName.TextColor;
-		sessionAdd = true;
-		NewFirearmCaliber.Text = VM.Caliber;
-	}
-	private bool sessionAdd = false;
-	private ViewModel.NewFirearmPageViewModel VM;
-	private Color? textColor = null;
-	private bool AddFirearmParamtersPass = false;
-	private bool edit_mode = false;
-	private string? edit_firearm_name;
-	private Models.Firearm? firearm;
+    /// <summary>
+    /// Checks input of name text and changes text color, sets error message, 
+    /// and disables save button if it is bad input
+    /// </summary>
+    void NewFirearmNameTextChanged(object sender, EventArgs e)
+    {
+        // name to long
+        if (NewFirearmName.Text.Length > 50)
+        {
 
-	private void PopulateWithFirearm(string name)
-	{
-        edit_firearm_name = name;
-        edit_mode = true;
-        firearm = App.FirearmRepo.GetFirearmFromName(edit_firearm_name);
-        NewFirearmName.Text = firearm.Name;
-		NewFirearmBarrelLength.Text = firearm.BarrelLength.ToString();
-		NewFirearmManufacturer.Text = firearm.Manufacturer;
-		NewFirearmCaliber.Text = firearm.Caliber;
-		NewFirearmScope.Text = firearm.ScopeID;
-	}
-	private bool NewFirearmNamePass = false;
-	void NewFirearmNameTextChanged(object sender, EventArgs e)
-	{
-		if (NewFirearmName.Text.Length > 50)
-		{
-
-			VM.SetStatusMessage("Name To Long");
+            VM.SetStatusMessage("Name To Long");
             NewFirearmName.TextColor = Colors.Red;
             NewFirearmNamePass = false;
-			if (AddFirearmParamtersPass)
-			{
-				saveFirearmButton.IsEnabled = false;
-				AddFirearmParamtersPass = false;
-			}
-		}
-		else if (NewFirearmName.Text.Length <= 0)
-		{
-			NewFirearmName.Placeholder = "Enter Name For New FirearmName";
-			VM.SetStatusMessage("");
-			NewFirearmNamePass = false;
-			if (AddFirearmParamtersPass)
-			{
-				saveFirearmButton.IsEnabled = false;
-				AddFirearmParamtersPass = false;
-			}
-		}
-		else
-		{
-			if (VM.CheckNameIsDuplicate(NewFirearmName.Text))
-			{
-				// in edit mode so check duplicate is not itself
-				if (edit_mode && edit_firearm_name == NewFirearmName.Text)
-				{
-					if (NewFirearmName.TextColor != textColor)
-						NewFirearmName.TextColor = textColor;
-					NewFirearmNamePass = true;
-					VM.SetStatusMessage("");
-					if (!AddFirearmParamtersPass && !saveFirearmButton.IsEnabled)
-					{
-						if (AddFirearmPararmetersPassCheck())
-							saveFirearmButton.IsEnabled = true;
-					}
-				}
-				else
-				{
-					VM.SetStatusMessage("Name Already Exists");
-					NewFirearmName.TextColor = Colors.Red;
-					NewFirearmNamePass = false;
-					if (AddFirearmParamtersPass)
-					{
-						saveFirearmButton.IsEnabled = false;
-						AddFirearmParamtersPass = false;
-					}
-				}
-			}
-			else
-			{
-				if (NewFirearmName.TextColor != textColor)
-					NewFirearmName.TextColor = textColor;
-				NewFirearmNamePass = true;
-				VM.SetStatusMessage("");
-				if (!AddFirearmParamtersPass && !saveFirearmButton.IsEnabled)
-				{
-					if (AddFirearmPararmetersPassCheck())
-						saveFirearmButton.IsEnabled = true;
-				}
-			}
+            if (AddFirearmParamtersPass)
+            {
+                saveFirearmButton.IsEnabled = false;
+                AddFirearmParamtersPass = false;
+            }
         }
-	}
-	private bool NewFirearmBarrelLengthPass = false;
-	void NewFirearmBarrelLengthTextChanged(object sender, EventArgs e)
-	{
-		if (NewFirearmBarrelLength.Text.Length > 0)
-		{
-			try
-			{
-				var barrelLength = int.Parse(NewFirearmBarrelLength.Text);
-			}
-			catch 
-			{
-				VM.SetStatusMessage("Barrel Length Must Be a Number");
-				NewFirearmBarrelLength.TextColor = Colors.Red;
-				NewFirearmBarrelLengthPass = false;
-				return;
-			}
-			VM.SetStatusMessage("");
-			if (NewFirearmBarrelLength.TextColor != textColor)
-				NewFirearmBarrelLength.TextColor = textColor;
-			NewFirearmBarrelLengthPass = true;
-		}
-		else
-		{
-			NewFirearmBarrelLengthPass = false;
-			NewFirearmBarrelLength.Placeholder = "Enter Barrel Length"; 
-			VM.SetStatusMessage("");
-		}
-		return;
-    }
-	private bool NewFirearmManufactrurerPass = false;
-    void NewFirearmManufacturerTextChanged(object sender, EventArgs e)
-	{
-		if (NewFirearmManufacturer.Text.Length > 30)
-		{
-			NewFirearmManufacturer.TextColor = Colors.Red;
-			VM.SetStatusMessage("Manufacture Name To Long");
-			NewFirearmManufactrurerPass = false;
-		}
-		else if (NewFirearmManufacturer.Text.Length < 1){
-			VM.SetStatusMessage("");
-			NewFirearmManufacturer.Placeholder = "Enter Manufacturer";
-			NewFirearmManufactrurerPass = false;
-		}
-		else
-		{
-			VM.SetStatusMessage("");
-			if (NewFirearmManufacturer.TextColor != textColor)
-                NewFirearmManufacturer.TextColor = textColor;
-			NewFirearmManufactrurerPass = true;
+        // name to short
+        else if (NewFirearmName.Text.Length <= 0)
+        {
+            NewFirearmName.Placeholder = "Enter Name For New FirearmName";
+            VM.SetStatusMessage("");
+            NewFirearmNamePass = false;
+            if (AddFirearmParamtersPass)
+            {
+                saveFirearmButton.IsEnabled = false;
+                AddFirearmParamtersPass = false;
+            }
         }
-		
-	}
-	private bool NewFirearmCaliberPass = false;
-	void NewFirearmCaliberTextChanged(object sender, EventArgs e)
-	{
-		if (NewFirearmCaliber.Text.Length > 20)
-		{
-			NewFirearmCaliber.TextColor = Colors.Red;
-			VM.SetStatusMessage("Diameter Name To Long");
-			NewFirearmCaliberPass = false;
-		}
-		else if (NewFirearmCaliber.Text.Length < 1)
-		{
-			VM.SetStatusMessage("");
-			NewFirearmCaliber.Placeholder = "Enter Diameter";
-			NewFirearmCaliberPass = false;
-		}
-		else
-		{
-			VM.SetStatusMessage("");
-			if (NewFirearmCaliber.TextColor != textColor)
-                NewFirearmCaliber.TextColor = textColor;
-			NewFirearmCaliberPass = true;
-        }
-	}
-	private bool NewFirearmScopePass = false;
-	void NewFirearmScopeTextChanged(object sender, EventArgs e)
-	{
-		if (NewFirearmScope.Text.Length > 50)
-		{
-			NewFirearmScope.TextColor = Colors.Red;
-			VM.SetStatusMessage("Scope Name To Long");
-			NewFirearmScopePass = false;
-		}
-		else if (NewFirearmScope.Text.Length < 1)
-		{
-			VM.SetStatusMessage("");
-			NewFirearmScope.Placeholder = "Enter Scope Name";
-			NewFirearmScopePass = false;
-		}
-		else
-		{
-			VM.SetStatusMessage("");
-			if (NewFirearmScope.TextColor != textColor)
-                NewFirearmScope.TextColor = textColor;
-			NewFirearmScopePass = true;
-        }
-
-    }
-	private bool AddFirearmPararmetersPassCheck()
-	{
-		if (NewFirearmNamePass)
-		{ 
-			AddFirearmParamtersPass = true;
-			return true; 
-		}
-		else
-		{
-			AddFirearmParamtersPass = false;
-			return false;
-		} 
-	}
-	private void SaveFirearm(object sender, EventArgs e)
-	{
-		if (!AddFirearmPararmetersPassCheck())
-			return;
-
-		int barrelLength;
-		if (NewFirearmBarrelLength.Text != null)
-            barrelLength = int.Parse(NewFirearmBarrelLength.Text);
+        // name length is good 
         else
-            barrelLength = 0;
-		var new_firearm = new Firearm
-		{
-			Name = NewFirearmName.Text,
-			BarrelLength = barrelLength,
-			Manufacturer = NewFirearmManufacturer.Text,
-			Caliber = NewFirearmCaliber.Text,
-			ScopeID = NewFirearmScope.Text,
-		};
-		if (firearm != null)
-		{
-			new_firearm.Id = firearm.Id;
-		}
-
-
-        App.FirearmRepo.AddNewFirearm(new_firearm);
-
-		if (!sessionAdd)
-		{
-			WeakReferenceMessenger.Default.Send(new SendItemMessage(NewFirearmName.Text));
-			Navigation.PopAsync();
-		}
-		else
-		{
-            var navigationParamenter = new Dictionary<string, object>
-			{
-			    {"firearm", new_firearm }
-			};
-            Shell.Current.GoToAsync("..", navigationParamenter);
+        {
+            // duplicate name check in database
+            if (VM.CheckNameIsDuplicate())
+            {
+                // in edit mode so check duplicate is not itself
+                var name = VM.getFirearmName();
+                if (name != null && name == NewFirearmName.Text)
+                {
+                    if (NewFirearmName.TextColor != textColor)
+                        NewFirearmName.TextColor = textColor;
+                    NewFirearmNamePass = true;
+                    VM.SetStatusMessage("");
+                    if (!AddFirearmParamtersPass && !saveFirearmButton.IsEnabled)
+                    {
+                        if (AddFirearmPararmetersPassCheck())
+                            saveFirearmButton.IsEnabled = true;
+                    }
+                }
+                // name is a duplicate
+                else
+                {
+                    VM.SetStatusMessage("Name Already Exists");
+                    NewFirearmName.TextColor = Colors.Red;
+                    NewFirearmNamePass = false;
+                    if (AddFirearmParamtersPass)
+                    {
+                        saveFirearmButton.IsEnabled = false;
+                        AddFirearmParamtersPass = false;
+                    }
+                }
+            }
+            // name is good
+            else
+            {
+                if (NewFirearmName.TextColor != textColor)
+                    NewFirearmName.TextColor = textColor;
+                NewFirearmNamePass = true;
+                VM.SetStatusMessage("");
+                if (!AddFirearmParamtersPass && !saveFirearmButton.IsEnabled)
+                {
+                    if (AddFirearmPararmetersPassCheck())
+                        saveFirearmButton.IsEnabled = true;
+                }
+            }
         }
     }
-	private void CancelSaveFirearm(object sender, EventArgs e)
-	{
-		Shell.Current.GoToAsync("..");
-	}
+
+    /// <summary>
+    /// Checks that the BarrelLength field is valid and sets the save button to clickable
+    /// if it is valid
+    /// </summary>
+    private bool NewFirearmBarrelLengthPass = false;
+    void NewFirearmBarrelLengthTextChanged(object sender, EventArgs e)
+    {
+        if (NewFirearmBarrelLength.Text.Length > 0)
+        {
+            try
+            {
+                var barrelLength = int.Parse(NewFirearmBarrelLength.Text);
+            }
+            catch
+            {
+                VM.SetStatusMessage("Barrel Length Must Be a Number");
+                NewFirearmBarrelLength.TextColor = Colors.Red;
+                NewFirearmBarrelLengthPass = false;
+                return;
+            }
+            VM.SetStatusMessage("");
+            if (NewFirearmBarrelLength.TextColor != textColor)
+                NewFirearmBarrelLength.TextColor = textColor;
+            NewFirearmBarrelLengthPass = true;
+        }
+        else
+        {
+            NewFirearmBarrelLengthPass = false;
+            NewFirearmBarrelLength.Placeholder = "Enter Barrel Length";
+            VM.SetStatusMessage("");
+        }
+        return;
+    }
+    /// <summary>
+    /// Checks that the Manufacture field is valid and sets the save button to clickable
+    /// if it is valid
+    /// </summary>
+    private bool NewFirearmManufactrurerPass = false;
+    void NewFirearmManufacturerTextChanged(object sender, EventArgs e)
+    {
+        if (NewFirearmManufacturer.Text.Length > 30)
+        {
+            NewFirearmManufacturer.TextColor = Colors.Red;
+            VM.SetStatusMessage("Manufacture Name To Long");
+            NewFirearmManufactrurerPass = false;
+        }
+        else if (NewFirearmManufacturer.Text.Length < 1)
+        {
+            VM.SetStatusMessage("");
+            NewFirearmManufacturer.Placeholder = "Enter Manufacturer";
+            NewFirearmManufactrurerPass = false;
+        }
+        else
+        {
+            VM.SetStatusMessage("");
+            if (NewFirearmManufacturer.TextColor != textColor)
+                NewFirearmManufacturer.TextColor = textColor;
+            NewFirearmManufactrurerPass = true;
+        }
+
+    }
+
+    /// <summary>
+    /// Checks that the Caliber field is valid and sets the save button to clickable
+    /// if it is valid
+    /// </summary>
+    private bool NewFirearmCaliberPass = false;
+    void NewFirearmCaliberTextChanged(object sender, EventArgs e)
+    {
+        if (NewFirearmCaliber.Text.Length > 20)
+        {
+            NewFirearmCaliber.TextColor = Colors.Red;
+            VM.SetStatusMessage("Diameter Name To Long");
+            NewFirearmCaliberPass = false;
+        }
+        else if (NewFirearmCaliber.Text.Length < 1)
+        {
+            VM.SetStatusMessage("");
+            NewFirearmCaliber.Placeholder = "Enter Diameter";
+            NewFirearmCaliberPass = false;
+        }
+        else
+        {
+            VM.SetStatusMessage("");
+            if (NewFirearmCaliber.TextColor != textColor)
+                NewFirearmCaliber.TextColor = textColor;
+            NewFirearmCaliberPass = true;
+        }
+    }
+
+    /// <summary>
+    /// Checks that the Scope field is valid and sets the save button to clickable
+    /// if it is valid
+    /// </summary>
+    private bool NewFirearmScopePass = false;
+    void NewFirearmScopeTextChanged(object sender, EventArgs e)
+    {
+        if (NewFirearmScope.Text.Length > 50)
+        {
+            NewFirearmScope.TextColor = Colors.Red;
+            VM.SetStatusMessage("Scope Name To Long");
+            NewFirearmScopePass = false;
+        }
+        else if (NewFirearmScope.Text.Length < 1)
+        {
+            VM.SetStatusMessage("");
+            NewFirearmScope.Placeholder = "Enter Scope Name";
+            NewFirearmScopePass = false;
+        }
+        else
+        {
+            VM.SetStatusMessage("");
+            if (NewFirearmScope.TextColor != textColor)
+                NewFirearmScope.TextColor = textColor;
+            NewFirearmScopePass = true;
+        }
+
+    }
+    private bool AddFirearmPararmetersPassCheck()
+    {
+        if (NewFirearmNamePass)
+        {
+            AddFirearmParamtersPass = true;
+            return true;
+        }
+        else
+        {
+            AddFirearmParamtersPass = false;
+            return false;
+        }
+    }
 }
